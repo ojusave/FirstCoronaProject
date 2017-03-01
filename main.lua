1c53c51bcdebcb3038079498d55ce06be5e71937
Ojus <ojus@theneura.com>
2017-02-28 16:54:47 -0800

    new commit
    added a reset button!

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@
 
 -- Your code here
 local tapCount = 0
+local widget = require "widget"
 
 local background = display.newImageRect( "background.jpg", 700,1075)
 background.x = display.contentCenterX
@@ -29,6 +30,26 @@ physics.start()
 physics.addBody( platform, "static" )
 physics.addBody( balloon, "dynamic", { radius=50, bounce=0.3 } )
 
+local myButton=widget.newButton{
+        --The id can be used to tell you what button was pressed in your button event
+	id = "myTextButton",
+        --This is the text label to put on the button
+	label = "Reset",
+        --This is the start x coordinate of the Top Left Corner
+	left=150,
+        --This is the start y coordinate of the Top Left Corner
+	top=150,
+        --Emboss can be true or false, tells it to make the label look embossed/inset
+	emboss=true,
+        --The border around the outside of the button
+	strokeWidth=4,
+        --How round to make the corners
+	cornerRadius=8,
+	--this tells it what function to call when you press the button
+        onEvent = myButtonEvent
+}
+
+
 local function pushBalloon()
   balloon:applyLinearImpulse( 0, -0.75, balloon.x, balloon.y )
   tapCount = tapCount + 1
@@ -37,3 +58,4 @@ local function pushBalloon()
 end
 
 balloon:addEventListener( "tap", pushBalloon )
+
